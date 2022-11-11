d08ba596555bfca5f684fb701c372492c6a2c523
Samilly-Vitalino <116197715+Samilly-Vitalino@users.noreply.github.com>
2022-11-11 15:26:51 -0300

    Projeto social

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function darkMode(){
     app.classList.toggle('dark')
 
 }
+
+const iconshow=document.querySelector('.show')
+const iconblock=document.querySelector('.block')
+const input=document.querySelector('#senha')
+
+function showPass(){
+    if(input.type ==='password'){
+        input.type='text'
+        console.log(iconshow);
+        iconshow.style.display='none'
+        iconblock.style.display='block'
+    } else{
+        input.type='password'
+        iconshow.style.display='block'
+        iconblock.style.display='none'
+    }
+
+}
+
